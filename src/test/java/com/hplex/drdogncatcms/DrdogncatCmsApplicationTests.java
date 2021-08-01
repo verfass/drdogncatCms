bf8e7910c88adb0b423ec5320fbbeead2620b0b8
verfass <verfass@nate.com>
2021-08-01 13:30:04 +0900

    Initial commit

---
+++ src/test/java/com/hplex/drdogncatcms/DrdogncatCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hplex.drdogncatcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrdogncatCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
