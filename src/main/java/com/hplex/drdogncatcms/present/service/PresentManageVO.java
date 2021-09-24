9ba67cf1374b74d041012fad5f548407955faa5e
verfass <verfass@nate.com>
2021-09-24 14:42:02 +0900

    20210924_1441

--- src/main/java/com/hplex/drdogncatcms/present/service/PresentManageVO.java
+++ src/main/java/com/hplex/drdogncatcms/present/service/PresentManageVO.java
@@ -12,17 +12,36 @@ import lombok.ToString;
 @NoArgsConstructor
 public class PresentManageVO extends PresentDefaultVO {
 
-    private static final long serialVersionUID = -1390939010587949531L;
+    private static final long serialVersionUID = -3407759472136774862L;
 
     private String seq;
+    private String memberSeq;
+    private String petId;
+    private String presentGubun;
+    private String expirationDt;
+    private String challengeId;
+    private String presentType;
+    private String presentCode;
     private String presentName;
-    private String presentPrice;
-    private String presentImg;
-    private String presentLink;
-    private String presentOrd;
+    private String presentNum;
+    private String reviewLink;
+    private String receiveName;
+    private String receivePhonenumber;
+    private String receivePost;
+    private String receiveAddress1;
+    private String receiveAddress2;
+    private String deliveryMemo;
+    private String deliveryCode;
+    private String deliverySearch;
+    private String stateStep;
+    private String rejectCode;
+    private String rejectMemo;
     private String chkAt;
     private String delAt;
     private String insDttm;
     private String uptDttm;
+    private String checkUser;
+    private String checkDttm;
+
 
 }
